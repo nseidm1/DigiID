8898c63d2ff7d91083ad9dc9c3bc736935da6af6
Noah <noah@noahseidman.com>
2019-03-21 18:17:51 -0400

    Add a backup option to show the readable mnemonic phrase.

--- app/src/main/java/com/noahseidman/digiid/QRCodeActivity.java
+++ app/src/main/java/com/noahseidman/digiid/QRCodeActivity.java
@@ -4,9 +4,14 @@ import android.Manifest;
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.provider.MediaStore;
 import android.view.View;
 import androidx.annotation.NonNull;
@@ -19,10 +24,14 @@ import com.crashlytics.android.Crashlytics;
 import com.firebase.ui.auth.AuthUI;
 import com.noahseidman.digiid.databinding.ActivityQrCodeBinding;
 import com.noahseidman.digiid.listeners.ActivityQRCodeCallback;
+import com.noahseidman.digiid.listeners.OnAdIdUpdateListener;
 import com.noahseidman.digiid.listeners.SaveListener;
 import com.noahseidman.digiid.utils.AnimatorHelper;
 import com.noahseidman.digiid.utils.FireBaseUtils;
 import com.noahseidman.digiid.utils.QRUtils;
+import com.pvryan.easycrypt.ECResultListener;
+import com.pvryan.easycrypt.symmetric.ECSymmetric;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
 import java.util.List;
@@ -90,11 +99,12 @@ public class QRCodeActivity extends AppCompatActivity implements View.OnLongClic
     public boolean onLongClick(View v) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(R.string.Options);
-        CharSequence[] items = new CharSequence[2];
+        CharSequence[] items = new CharSequence[3];
         items[0] = getString(R.string.SaveQR);
         items[1] = getString(R.string.Google);
-        builder.setItems(items, (dialog, item) -> {
-            switch (item) {
+        items[2] = getString(R.string.ShowSeedPhrase);
+        builder.setItems(items, (d, i) -> {
+            switch (i) {
                 case 0: {
                     if (ContextCompat
                             .checkSelfPermission(QRCodeActivity.this,
@@ -117,6 +127,44 @@ public class QRCodeActivity extends AppCompatActivity implements View.OnLongClic
                             .setAvailableProviders(providers)
                             .build(), FIREBASE_CODE);
                     break;
+                }
+                case 2: {
+                    FireBaseUtils.Companion.updateAdId(QRCodeActivity.this, new OnAdIdUpdateListener() {
+                        @Override
+                        public void onComplete() {
+                            if (FireBaseUtils.Companion.getAdvertisingId() != null) {
+                                ECSymmetric eCryptSymmetric = new ECSymmetric();
+                                eCryptSymmetric.decrypt(phrase, FireBaseUtils.Companion.getAdvertisingId(), new ECResultListener() {
+                                    @Override public void onProgress(int i, long l, long l1) {}
+                                    @Override
+                                    public <T> void onSuccess(T t) {
+                                        new Handler(Looper.getMainLooper()).post(() -> {
+                                            AlertDialog.Builder showBuilder = new AlertDialog.Builder(QRCodeActivity.this);
+                                            showBuilder.setTitle(R.string.ShowSeedPhrase);
+                                            showBuilder.setIcon(R.drawable.ic_digiid);
+                                            showBuilder.setMessage(t.toString());
+                                            showBuilder.setNegativeButton(android.R.string.cancel, (dialog, which) -> {});
+                                            showBuilder.setPositiveButton(R.string.Copy, (dialog, which) -> {
+                                                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                                                ClipData clip = ClipData.newPlainText(getString(R.string.DigiIDSeedPhrase), t.toString());
+                                                clipboard.setPrimaryClip(clip);
+                                                NotificationFragment.show(QRCodeActivity.this, getString(R.string.CopiedToClipboard), "", R.raw.success_check, null);
+                                            });
+                                            showBuilder.show();
+                                        });
+                                    }
+                                    @Override
+                                    public void onFailure(@NotNull String s, @NotNull Exception e) {
+                                        NotificationFragment.show(QRCodeActivity.this, getString(R.string.CopyFailed), "", R.raw.error_check, null);
+                                    }
+                                });
+                            }
+                        }
+                        @Override
+                        public void onFailure() {
+                            NotificationFragment.show(QRCodeActivity.this, getString(R.string.CopyFailed), "", R.raw.error_check, null);
+                        }
+                    });
                 }
             }
         });
